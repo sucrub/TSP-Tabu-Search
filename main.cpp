7b91dfc2cae8f32d95bc15793e1f60788224eb19
sucrub <bachspham@gmail.com>
2023-01-09 08:49:56 +0700

    Format the code

--- main.cpp
+++ main.cpp
@@ -19,19 +19,23 @@ int *sol = new int[MAX];
 /*
 	Read problem from file
 */
-void readFile() {
+void readFile()
+{
 
 	ifstream f;
 	f.open("test.txt");
 	f >> N;
 
 	graph = new int *[N];
-	for(int i = 0; i < N; i++) {
-		graph[i] = new int [N];
+	for (int i = 0; i < N; i++)
+	{
+		graph[i] = new int[N];
 	}
 
-	for(int i = 0; i < N; i++) {
-		for(int j = 0; j < N; j++) {
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
 			f >> graph[i][j];
 		}
 	}
@@ -41,11 +45,13 @@ void readFile() {
 /*
 	Caculated the length of the solution's route
 */
-int fitness(int *sol) {
+int fitness(int *sol)
+{
 
 	int path = 0; // Path's length
 
-	for (int i = 0; i < N - 1; i++) {
+	for (int i = 0; i < N - 1; i++)
+	{
 		path = path + graph[sol[i]][sol[i + 1]];
 	}
 
@@ -56,7 +62,8 @@ int fitness(int *sol) {
 /*
 	Create a first solution randomly
 */
-int *first_sol() {
+int *first_sol()
+{
 
 	srand(time(NULL));
 	int r;
@@ -68,8 +75,10 @@ int *first_sol() {
 	tmp[0] = 0;
 
 	// Random route
-	for (int i = 1; i < N; i++) {
-		do {
+	for (int i = 1; i < N; i++)
+	{
+		do
+		{
 			r = 1 + rand() % ((N - 1) + 1 - 1);
 		} while (vis.find(r) != vis.end());
 		vis[r] = true;
@@ -81,23 +90,27 @@ int *first_sol() {
 /*
 	Tabu Search
 */
-void tabuSearch() {
+void tabuSearch()
+{
 
 	int tabu_list[N]; // Tabu list
 	memset(tabu_list, N, 0);
 
 	sol = first_sol(); // Make a first solution
 
-	int best_val = fitness(sol); // Best value for this problem that tabu search can find 
+	int best_val = fitness(sol); // Best value for this problem that tabu search can find
 
-	int *best_sol = new int [MAX]; // Best solution for this problem that tabu search can find
-	for (int i = 0; i < N; i++) best_sol[i] = sol[i];
+	int *best_sol = new int[MAX]; // Best solution for this problem that tabu search can find
+	for (int i = 0; i < N; i++)
+		best_sol[i] = sol[i];
 
-	int *best_candidate = new int [MAX]; // Best solution from last loop
-	for (int i = 0; i < N; i++) best_candidate[i] = sol[i];
+	int *best_candidate = new int[MAX]; // Best solution from last loop
+	for (int i = 0; i < N; i++)
+		best_candidate[i] = sol[i];
 
-	int *neighbor_candidate = new int [MAX]; // Best solution for current neighbor 
-	for (int i = 0; i < N; i++) neighbor_candidate[i] = sol[i];
+	int *neighbor_candidate = new int[MAX]; // Best solution for current neighbor
+	for (int i = 0; i < N; i++)
+		neighbor_candidate[i] = sol[i];
 
 	bool stop = false; // stop condition
 
@@ -107,32 +120,40 @@ void tabuSearch() {
 
 	int stopping_turn = 1000; // If best_keeping == stopping_turn then stop = true
 
-	while(!stop) {
+	while (!stop)
+	{
 
 		int city1, city2;
 		int cmp = INT_MAX;
 
-		for(int i = 1; i < N; i++) {
-			for(int j = 1; j < N; j++) {
+		for (int i = 1; i < N; i++)
+		{
+			for (int j = 1; j < N; j++)
+			{
 
-				if(i == j) continue;
+				if (i == j)
+					continue;
 
-				int* tmp_sol = new int [MAX]; // Current solution 
-				for(int i = 0; i < N; i++) {
+				int *tmp_sol = new int[MAX]; // Current solution
+				for (int i = 0; i < N; i++)
+				{
 					tmp_sol[i] = best_candidate[i]; // Equal to the last loop's solution for base
 				}
 
-				swap(tmp_sol[i], tmp_sol[j]); // Swap two city to make a new route 
+				swap(tmp_sol[i], tmp_sol[j]); // Swap two city to make a new route
 
 				// Check the route if its tabu
 				// Doesn't tabu
-				if(tabu_list[i] == 0 && tabu_list[j] == 0) {
-					
-					// If this solution is currently the best one so far in this neighbor
-					if(fitness(tmp_sol) < cmp) {
+				if (tabu_list[i] == 0 && tabu_list[j] == 0)
+				{
 
-						// Update the value 
-						for (int i = 0; i < N; i++) neighbor_candidate[i] = tmp_sol[i];
+					// If this solution is currently the best one so far in this neighbor
+					if (fitness(tmp_sol) < cmp)
+					{
+
+						// Update the value
+						for (int i = 0; i < N; i++)
+							neighbor_candidate[i] = tmp_sol[i];
 						cmp = fitness(neighbor_candidate);
 						city1 = i;
 						city2 = j;
@@ -140,54 +161,64 @@ void tabuSearch() {
 				}
 
 				// Does tabu
-				else {
+				else
+				{
 
 					// Check if this solution is currently better than the best one so far of tabu search
-					if(fitness(tmp_sol) < best_val
-						&& fitness(tmp_sol) < cmp) {
+					if (fitness(tmp_sol) < best_val && fitness(tmp_sol) < cmp)
+					{
 
 						// Update the value
-						for (int i = 0; i < N; i++) neighbor_candidate[i] = tmp_sol[i];
+						for (int i = 0; i < N; i++)
+							neighbor_candidate[i] = tmp_sol[i];
 						cmp = fitness(neighbor_candidate);
 						city1 = i;
 						city2 = j;
 					}
 
 					// Discard it when it doesn't meet the requirement
-					else { 
-						delete[] tmp_sol; 
+					else
+					{
+						delete[] tmp_sol;
 						continue;
 					}
 				}
 
-			delete [] tmp_sol;
+				delete[] tmp_sol;
 			}
 		}
 
-		//After checking out all the neighbor, best one stay in neighbor_candidate, city1 and city2 is the two cities that swap
+		// After checking out all the neighbor, best one stay in neighbor_candidate, city1 and city2 is the two cities that swap
 
 		// Update the value
-		for (int i = 0; i < N; i++) 
+		for (int i = 0; i < N; i++)
 			best_candidate[i] = neighbor_candidate[i]; // Update the best value from the last loop
 
-		if(fitness(best_candidate) < best_val) { // If this solution is better than what we have so far
+		if (fitness(best_candidate) < best_val)
+		{ // If this solution is better than what we have so far
 
-			for (int i = 0; i < N; i++) best_sol[i] = best_candidate[i];
+			for (int i = 0; i < N; i++)
+				best_sol[i] = best_candidate[i];
 			best_val = fitness(best_sol);
 			best_keepping = -1;
 		}
 
 		// Update the tabu list
 
-		if(tabu_list[city1] == 0 && tabu_list[city2] == 0) { // If the solution is not tabu
-			for(int i = 0; i < N; i++) {
-				if(tabu_list[i] > 0) tabu_list[i]--;
+		if (tabu_list[city1] == 0 && tabu_list[city2] == 0)
+		{ // If the solution is not tabu
+			for (int i = 0; i < N; i++)
+			{
+				if (tabu_list[i] > 0)
+					tabu_list[i]--;
 			}
 			tabu_list[city1] += t;
 		}
 
-		else { // If the solution is tabu
-			for(int i = 0; i < N; i++) {
+		else
+		{ // If the solution is tabu
+			for (int i = 0; i < N; i++)
+			{
 				tabu_list[i] = 0;
 			}
 			tabu_list[city1] += t;
@@ -195,7 +226,8 @@ void tabuSearch() {
 
 		// Checking the stop_condition
 		best_keepping++;
-		if(best_keepping == stopping_turn) {
+		if (best_keepping == stopping_turn)
+		{
 			stop = true;
 		}
 	}
@@ -207,12 +239,13 @@ void tabuSearch() {
 		cout << best_sol[i] << " ";
 	cout << endl;
 
-	delete [] best_sol;
-	delete [] best_candidate;
-	delete [] neighbor_candidate;
+	delete[] best_sol;
+	delete[] best_candidate;
+	delete[] neighbor_candidate;
 }
 
-int main() {
+int main()
+{
 	readFile();
 	tabuSearch();
 
--- randomgraph.cpp
+++ randomgraph.cpp
@@ -1,37 +1,46 @@
-#include<bits/stdc++.h>
-#include<fstream>
+#include <bits/stdc++.h>
+#include <fstream>
 using namespace std;
 
 int N;
 int **graph;
 
-int main() {
+int main()
+{
 
     srand(time(NULL));
     N = rand() % (15 - 6 + 1) + 6;
     graph = new int *[N];
-    for(int i = 0; i < N; i++) {
-        graph[i] = new int [N];
+    for (int i = 0; i < N; i++)
+    {
+        graph[i] = new int[N];
     }
     ofstream f;
     f.open("test.txt");
     f << N << endl;
-    for(int i = 0; i < N; i++) {
-        for(int j = 0; j < N; j++) {
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
 
-            if(i == j) {
+            if (i == j)
+            {
                 graph[i][j] = 0;
                 graph[j][i] = 0;
             }
-            if(i > j) continue;
-            if(i < j) {
+            if (i > j)
+                continue;
+            if (i < j)
+            {
                 graph[i][j] = rand() % (20 - 1 + 1) + 1;
                 graph[j][i] = graph[i][j];
             }
         }
     }
-    for(int i = 0; i < N; i++) {
-        for(int j = 0; j < N; j++) {
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
             f << graph[i][j] << " ";
         }
         f << endl;
